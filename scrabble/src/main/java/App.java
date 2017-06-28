bee77d7cd5bec554650344641410f055a856a9c5
Joel Bakken and Lincoln Nguyen <joel@>
2017-06-28 14:29:30 -0700

    add logic to App.java

--- scrabble/src/main/java/App.java
+++ scrabble/src/main/java/App.java
@@ -1,13 +1,15 @@
-// import java.io.Console;
-// import java.util.List;
-//
-// public class App {
-//   public static void main(String[] args) {
-//     Console myConsole = System.console();
-//     // System.out.println("Enter a letter:");
-//     // String stringUserLetter = myConsole.readLine();
-//     // Scrabble test1 = new Scrabble();
-//     // List<Object> scrabbleResult = test1.calculateScore(stringUserLetter);
-//     // System.out.println(scrabbleResult);
-//   }
-// }
+import java.io.Console;
+import java.util.List;
+import java.util.ArrayList;
+
+public class App {
+  public static void main(String[] args) {
+    Console myConsole = System.console();
+    System.out.println("Enter a word:");
+    String stringUserLetter = myConsole.readLine();
+    Scrabble scrabble = new Scrabble();
+    Integer scrabbleResult = scrabble.calculateScore(stringUserLetter);
+
+    System.out.println(scrabbleResult);
+  }
+}
